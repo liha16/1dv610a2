b39407c03cc03ef78f5c7c11cb765be63118f3b8
Lisa Veltman Hellinger <lv222fz@student.lnu.se>
2020-09-15 15:30:21 +0200

    Fixed  DateTime output

--- src/view/DateTimeView.php
+++ src/view/DateTimeView.php
@@ -2,11 +2,14 @@
 
 class DateTimeView {
 
-
+	// Returns a paragraf element with a date with format:  
+	// Monday, the 8th of July 2015, The time is 10:59:21
+	
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$dateString = date("l") . ", the " . date("d") . "th of " . date("F") . " " . date("Y") . ",";
+		$timeString = "The time is " . date("h:i:s"); 
 
-		return '<p>' . $timeString . '</p>';
+		return '<p>' . $dateString . " " . $timeString . '</p>';
 	}
 }
